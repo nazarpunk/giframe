b06f46640f34af6b13247238b347ec03b4f9e97f
nazarpunk <nazarpunk@gmail.com>
2023-02-15 14:20:27 +0200

    sprite_test.mdx

--- main.mjs
+++ main.mjs
@@ -2,7 +2,7 @@
 
 import {Model} from "./model/Model.mjs";
 
-const response = await fetch('7x7sprite.mdx');
+const response = await fetch('mdx/sprite.mdx');
 const buffer = await response.arrayBuffer();
 
 new Model(buffer);

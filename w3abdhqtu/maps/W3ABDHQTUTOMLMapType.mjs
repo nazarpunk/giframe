86cd2415bbe676c1f69c576fb9f92f28c2d59760
nazarpunk <nazarpunk@gmail.com>
2023-06-28 00:12:14 +0300

    W3U toml map

--- w3abdhqtu/maps/W3ABDHQTUTOMLMapType.mjs
+++ w3abdhqtu/maps/W3ABDHQTUTOMLMapType.mjs
@@ -10,30 +10,6 @@ export default (type) => {
         case 'bool':
             itype = 0;
             break;
-        case 'abilCode':
-        case 'aiBuffer':
-        case 'attackBits':
-        case 'attributeType':
-        case 'armorType':
-        case 'channelType': //
-        case 'detectionType':
-        case 'deathType':
-        case 'defenseTypeInt':
-        case 'fullFlags':
-        case 'int':
-        case 'unitCode':
-        case 'upgradeCode':
-        case 'unitClass':
-        case 'morphFlags':
-        case 'regenType':
-        case 'stackFlags':
-        case 'itemClass':
-        case 'interactionFlags':
-        case 'channelFlags':
-        case 'pickFlags':
-        case 'silenceFlags':
-            itype = 0;
-            break;
         case 'real':
             itype = 1;
             break;
@@ -44,41 +20,66 @@ export default (type) => {
             itype = 3;
             singleline = false;
             break;
+        //
+        case 'abilCode':
+        case 'aiBuffer':
+        case 'attackBits':
+        case 'attributeType':
+        case 'armorType':
+        case 'channelType': //
+        case 'channelFlags':
+        case 'detectionType':
+        case 'deathType':
+        case 'defenseTypeInt':
+        case 'fullFlags':
+        case 'int':
+        case 'itemClass':
+        case 'interactionFlags':
+        case 'morphFlags':
+        case 'pickFlags':
+        case 'regenType':
+        case 'stackFlags':
+        case 'silenceFlags':
+        case 'unitClass':
+        case 'unitCode':
+        case 'upgradeCode':
+            itype = 0;
+            break;
         case 'abilityList':
         case 'abilitySkinList':
         case 'attackType':
         case 'buffList':
-        case 'effectList':
         case 'char':
         case 'combatSound':
         case 'defenseType': //
+        case 'effectList':
+        case 'heroAbilityList':
         case 'icon':
-        case 'soundLabel': //
-        case 'stringList':
+        case 'intList':
+        case 'itemList':
         case 'lightningList':
         case 'modelList':
-        case 'heroAbilityList':
+        case 'model':
+        case 'moveType':
+        case 'orderString':
         case 'pathingListPrevent':
         case 'pathingListRequire':
         case 'pathingTexture':
+        case 'soundLabel': //
+        case 'stringList':
         case 'shadowTexture':
-        case 'orderString':
+        case 'shadowImage':
         case 'teamColor':
         case 'uberSplat':
         case 'unitSound':
         case 'techList':
-        case 'shadowImage':
         case 'tilesetList':
-        case 'intList':
-        case 'itemList':
-        case 'versionFlags':
-        case 'model':
-        case 'moveType':
-        case 'unitRace':
         case 'targetList':
+        case 'unitRace':
         case 'unitList':
         case 'upgradeList':
         case 'unitSkinList':
+        case 'versionFlags':
         case 'weaponType':
             itype = 3;
             break;
